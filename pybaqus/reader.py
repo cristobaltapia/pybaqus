83e7f38cd56e259db955b2a50a8758ceb91a6560
Cristóbal Tapia Camú <crtapia@gmail.com>
2021-07-27 16:27:48 +0200

    fix: free memory during the import process

--- pybaqus/reader.py
+++ pybaqus/reader.py
@@ -27,14 +27,17 @@ def open_fil(file_name):
     # Remove new lines to get one continuous string
     lines = [li.replace("\n", "") for li in lines]
     res_line = "".join(lines)
+    del lines
 
     # Make a list with each record.
     # Each record starts with `*`, so we just need to use a regex to return each
     # record.
     pattern = r"(?<=\*)+.+?(?=\*)|(?<=\*)+.+?(?=$)"
     records = re.findall(pattern, res_line)
+    del res_line
 
     # Create result object
     result = FilParser(records)
+    del records
 
     return result.model
