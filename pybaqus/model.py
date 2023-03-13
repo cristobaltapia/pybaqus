fix: bug get_node_coords if node ids are not consecutive and starting from 1 (e.g. first node 1 deleted or unused for the calculation and .fil first node id==2), create in any case the kmap to allow be returned to map ids of nodes of elements obtained with get_cells and with ids of nodal results. Added comments to def cell also, could replace the ids uncommenting them, or replace it outside knowing the kmap (and probalby the same must be done for nodal result ids)

pybaqus/model.py
@@ -618,8 +618,13 @@ class Model:
             # Map new indices to old indices
             kmap = {k: ix for k, ix in zip(keys, old_keys)}
         else:
-            keys = sorted(list(nodes.keys()))
-            kmap = {k: k for k in keys}
+            #keys = sorted(list(nodes.keys()))
+            #kmap = {k: k for k in keys}
+            #the previous assumes that nodes are from 1 to n without jumps!!
+            old_keys = sorted(list(nodes.keys()))
+            keys = np.arange(1, len(old_keys) + 1, 1)
+            # Map new indices to old indices
+            kmap = {k: ix for k, ix in zip(keys, old_keys)}
 
         coords = np.empty((len(keys), 3))
 
@@ -702,6 +707,12 @@ class Model:
             elements = {k: elements[k] for k in elem_ids}
         else:
             kmap = None
+            #
+            #can replace here the kmap used for get_node_coords of do it outside knowing kmap 
+            #node_ids = sorted(list(self.nodes.keys()))
+            #new_node_ids = np.arange(1, len(node_ids) + 1, 1)
+            ## Map new indices to old indices
+            #kmap = {ix: k for k, ix in zip(new_node_ids, node_ids)}
 
         keys = sorted(list(elements.keys()))
 