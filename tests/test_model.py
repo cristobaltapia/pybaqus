8e7d75a384c77a0dc8db163887d48f8c73ad1cb6
Cristóbal Tapia Camú <crtapia@gmail.com>
2024-11-08 00:32:18 +0100

    feat: store nodes in a numpy array

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,32 +1,39 @@
 import pytest
+
 from pybaqus.model import Model
 from pybaqus.nodes import Node2D, Node3D
+
 
 @pytest.fixture
 def empty_model():
     return Model()
 
+
 # @pytest.fixture
 # def new_2D_node():
 #     return Node2D(1, dof_map=[])
 
+
 def test_model_initialization(empty_model):
     assert isinstance(empty_model, Model)
-    assert len(empty_model.nodes) == 0
+    assert empty_model.nodes == None
     assert len(empty_model.elements) == 0
     assert len(empty_model.element_sets) == 0
     assert len(empty_model.node_sets) == 0
     assert len(empty_model.surfaces) == 0
+
 
 def test_add_node(empty_model):
     class MockNode:
         def __init__(self, num):
             self._num = num
 
-    node = MockNode(1)
+    node = MockNode(0)
+    empty_model.size = (1, 1)
     empty_model.add_node(node)
     assert len(empty_model.nodes) == 1
-    assert empty_model.nodes[1] == node
+    assert empty_model.nodes[0] == node
+
 
 def test_add_element(empty_model):
     class MockElement:
@@ -37,6 +44,7 @@ def test_add_element(empty_model):
     empty_model.add_element(element)
     assert len(empty_model.elements) == 1
     assert empty_model.elements[1] == element
+
 
 def test_add_set(empty_model):
     elements = [1, 2, 3]
@@ -50,4 +58,3 @@ def test_add_set(empty_model):
     assert "test_node_set" in empty_model.node_sets
     assert empty_model.node_sets["test_node_set"] == nodes
     assert result == nodes
-
