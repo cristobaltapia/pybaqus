98c02c0a66bce1bdc79a8bc0ddcb76b41cdc575e
Cristóbal Tapia Camú <crtapia@gmail.com>
2024-11-09 14:53:00 +0100

    test: add tests for 3D models

--- tests/test_output.py
+++ tests/test_output.py
@@ -42,3 +42,53 @@ def test_output_2D_element_set(fil_path_2d):
         var="S2", step=1, inc=1, elem_set="ASSEMBLY_TEST_INSTANCE_SET-TEST_PART"
     )
     assert_allclose(displ, [1562.5, 1562.5, 1562.5, 1562.5])
+
+
+def test_output_3D_node_set(fil_path_3d):
+    """Test output for a node set.
+
+    Parameters
+    ----------
+        fil_path_2d:
+            The path to the *.fil file.
+    """
+    model = open_fil(fil_path_3d)
+    displ = model.get_nodal_result(
+        var="U2", step=1, inc=1, node_set="ASSEMBLY_SET_LOAD"
+    )
+    assert_allclose(
+        displ,
+        [
+            0.05991461924418786,
+            0.0582193538221954,
+            0.05027534615199374,
+            0.0551842083097384,
+        ],
+    )
+
+
+def test_output_3D_element_set(fil_path_3d):
+    """Test output for an element set.
+
+    Parameters
+    ----------
+        fil_path_3d:
+            The path to the *.fil file.
+    """
+    model = open_fil(fil_path_3d)
+    displ = model.get_nodal_result(
+        var="S2", step=1, inc=1, elem_set="ASSEMBLY_TEST_INSTANCE_SET-TEST_PART"
+    )
+    assert_allclose(
+        displ,
+        [
+            26.275343053264052,
+            -38.063847948411876,
+            78.43215874816171,
+            13.046268627606647,
+            -48.20076875561976,
+            -16.610991953481744,
+            3.9560469392777455,
+            34.49912462253665,
+        ],
+    )
